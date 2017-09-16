cc4f1dafa248b538b0b7ff89bfc52b0ed6ffa9ed
PrasanthSatz <prasanthsatz91@hotmail.com>
2017-09-16 06:54:04 -0700

    Adding Missed out Files

---
+++ plot4.R
@@ -0,0 +1,37 @@
+filename <- "EPC.zip"
+
+## Downloading Electric Power Consumption Data from UCI Machine Learning Repository
+library(sqldf)
+
+if(!file.exists(filename)) {
+  fileurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  download.file(fileurl,filename,mode="wb")
+}
+
+## Unzipping the Downloaded File
+
+if(!file.exists("household_power_consumption.txt"))
+  unzip(filename)
+
+data <- read.csv.sql("./household_power_consumption.txt",sep=";",sql="select * from file where Date in ('1/2/2007','2/2/2007')")
+
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+data$Time <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S")
+
+##Plotting the Plot4.png
+png("plot4.png",width=480,height=480,units="px")
+
+par(mfrow=c(2,2))
+
+plot(data$Time,data$Global_active_power,type="l",xlab="",ylab="Global Active Power(kilowatts)")
+
+plot(data$Time,data$Voltage,type="l",xlab="datetime",ylab="Voltage")
+
+plot(data$Time, data$Sub_metering_1,type="l",xlab="",ylab="Energy Sub Metering")
+lines(data$Time, data$Sub_metering_2, type="l",col="red")
+lines(data$Time, data$Sub_metering_3, type="l",col="blue")
+legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=1, col=c("black","red","blue"))
+
+plot(data$Time,data$Global_reactive_power,type="l",xlab="datetime",ylab="Global_reactive_power")
+
+dev.off()
